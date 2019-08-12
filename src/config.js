7410420d50ade8af5ae1d8299d9064e9aa115355
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2019-08-13 01:03:27 +0200

    Making complete registration and manage trainee responsive

--- src/config.js
+++ src/config.js
@@ -40,16 +40,30 @@ const operatorsAliases = {
 let config = module.exports;
 
 config.db = {
-  user: 'izxujd4earcd8i2r',
-  password: 'meul94fl14guwixl',
-  name: 'syy0w773umxa68su'
+  user: 'root', 
+  password: '',
+  name: 'rats_gym'
 };
 config.db.details = {
-  host: 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port: 3306,
+  host: 'localhost',
   dialect: 'mysql',
   define: {
     timestamps: false
   },
   operatorsAliases
 };
+
+// config.db = {
+//   user: 'toro',
+//   password: 'torotoro',
+//   name: 'toro'
+// };
+// config.db.details = {
+//   host: 'tor.cnhabkvxjgoz.us-east-2.rds.amazonaws.com',
+//   port: 3306,
+//   dialect: 'mysql',
+//   define: {
+//     timestamps: false
+//   },
+//   operatorsAliases
+// };
